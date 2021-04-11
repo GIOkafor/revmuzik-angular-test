6106c37e4dbddc38ecdcf00a64a240010523d0b8
GIOkafor <ikechukwuokafor@hotmail.co.uk>
2021-04-11 11:46:03 -0400

    removed API key

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { HttpClientModule } from '@angular/common/http';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { HttpClientModule } from "@angular/common/http";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBf4BIO3N0ljcs4SIAJIxruzRdjYWJ8fPw",
-    authDomain: "posingapp.firebaseapp.com",
-    databaseURL: "https://posingapp.firebaseio.com",
-    projectId: "posingapp",
-    storageBucket: "posingapp.appspot.com",
-    messagingSenderId: "857962289964",
-    appId: "1:857962289964:web:09c881cb1292de48"
-  };
+  apiKey: "",
+  authDomain: "posingapp.firebaseapp.com",
+  databaseURL: "https://posingapp.firebaseio.com",
+  projectId: "posingapp",
+  storageBucket: "posingapp.appspot.com",
+  messagingSenderId: "857962289964",
+  appId: "1:857962289964:web:09c881cb1292de48"
+};
 
 @NgModule({
-  imports:      [ 
-    BrowserModule, 
-    FormsModule, 
+  imports: [
+    BrowserModule,
+    FormsModule,
     HttpClientModule,
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFirestoreModule
   ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
